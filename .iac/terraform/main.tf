717b116cd0a2ebb50e4f6da8f875a62b61314503
Oliver Höhn <oliverphiliphohn@gmail.com>
2021-12-30 21:10:52 +0000

    Remove deprecated GCP bucket to store recipes
    Why?
    - Has been replaced by storing recipes on a local DB.

--- .iac/terraform/main.tf
+++ .iac/terraform/main.tf
@@ -3,36 +3,3 @@ provider "google" {
   region  = "europe-west1"
   zone    = "europe-west1-b"
 }
-
-resource "google_storage_bucket" "datastore" {
-    name = "mealfriend-datastore"
-    location = "US-EAST1"
-    uniform_bucket_level_access = true
-    versioning {
-      enabled = true
-    }
-
-    force_destroy = true
-}
-
-resource "google_service_account" "datastore_account" {
-  account_id   = "datastore"
-  display_name = "Datastore Account"
-}
-
-resource "google_storage_bucket_iam_member" "read_and_write_to_datastore_iam" {
-  bucket = google_storage_bucket.datastore.name
-  role = "roles/storage.objectAdmin"
-  member = "serviceAccount:${google_service_account.datastore_account.email}"
-
-  condition {
-    title       = "datastore_only"
-    expression  = <<CONDITION
-      resource.type == "storage.googleapis.com/Bucket" && resource.name == "${google_storage_bucket.datastore.url}"
-    CONDITION
-  }
-}
-
-resource "google_service_account_key" "datastore_account_key" {
-  service_account_id = google_service_account.datastore_account.name
-}
--- .iac/terraform/outputs.tf
+++ .iac/terraform/outputs.tf
@@ -1,8 +1,1 @@
-output "datastore_account_email" {
-    value = google_service_account.datastore_account.email
-}
 
-output "datastore_account_key" {
-    value = google_service_account_key.datastore_account_key.private_key
-    sensitive = true
-}
