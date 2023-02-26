d4cf6af8342e0ad82b6f6e9b9a2c20a23cb70b74
Oliver Höhn <oliverphiliphohn@gmail.com>
2023-02-26 21:15:44 +0000

    Add helper script to build training data for GCP

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/jinzhu/now v1.1.2 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
--- models/ingredient.go
+++ models/ingredient.go
@@ -4,6 +4,19 @@ import (
 	"gorm.io/gorm"
 )
 
+const (
+	BEEF               = "beef"
+	DAIRY_AND_EGG      = "dairy_and_egg"
+	FRUIT              = "fruit"
+	GRAIN_AND_PASTA    = "grain_and_pasta"
+	LAMB_VEAL_AND_GAME = "lamb_veal_and_game"
+	LEGUMES            = "legumes"
+	PORK               = "pork"
+	POULTRY            = "poultry"
+	SHELLFISH          = "shellfish"
+	VEGETABLE          = "vegetable"
+)
+
 type Ingredient struct {
 	gorm.Model
 	Name string
