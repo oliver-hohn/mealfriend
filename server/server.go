37e6d972dc6b14391ebf9c1a06e55fa09405a8de
Oliver Höhn <oliverphiliphohn@gmail.com>
2023-08-07 16:07:49 +0100

    Log gRPC requests (#9)

--- server/server.go
+++ server/server.go
@@ -7,12 +7,14 @@ import (
 	"net"
 	"net/url"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	"github.com/oliver-hohn/mealfriend/envs"
 	"github.com/oliver-hohn/mealfriend/graph"
 	"github.com/oliver-hohn/mealfriend/models"
 	pb "github.com/oliver-hohn/mealfriend/protos"
 	"github.com/oliver-hohn/mealfriend/scrapers"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
@@ -123,7 +125,51 @@ func toProtoTag(t models.Tag) pb.Tag {
 	}
 }
 
+func InterceptorLogger(l *zap.Logger) logging.Logger {
+	return logging.LoggerFunc(func(ctx context.Context, lvl logging.Level, msg string, fields ...any) {
+		f := make([]zap.Field, 0, len(fields)/2)
+
+		for i := 0; i < len(fields); i += 2 {
+			key := fields[i]
+			value := fields[i+1]
+
+			switch v := value.(type) {
+			case string:
+				f = append(f, zap.String(key.(string), v))
+			case int:
+				f = append(f, zap.Int(key.(string), v))
+			case bool:
+				f = append(f, zap.Bool(key.(string), v))
+			default:
+				f = append(f, zap.Any(key.(string), v))
+			}
+		}
+
+		logger := l.WithOptions(zap.AddCallerSkip(1)).With(f...)
+
+		switch lvl {
+		case logging.LevelDebug:
+			logger.Debug(msg)
+		case logging.LevelInfo:
+			logger.Info(msg)
+		case logging.LevelWarn:
+			logger.Warn(msg)
+		case logging.LevelError:
+			logger.Error(msg)
+		default:
+			panic(fmt.Sprintf("unknown level %v", lvl))
+		}
+	})
+}
+
 func main() {
+	logger := zap.NewExample()
+
+	opts := []logging.Option{
+		logging.WithLogOnEvents(logging.StartCall, logging.FinishCall),
+		// Add any other option (check functions starting with logging.With).
+	}
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50051))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -136,7 +182,11 @@ func main() {
 	}
 	defer driver.Close(ctx)
 
-	s := grpc.NewServer()
+	s := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			logging.UnaryServerInterceptor(InterceptorLogger(logger), opts...),
+		),
+	)
 	pb.RegisterMealfriendServer(s, &mealfriendServer{driver: driver})
 	reflection.Register(s)
 
